5d1dd76d232a940390c30407e47acd2b2b068ba8
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-07 14:43:49 +0530

    feat(server): add MongoDB connection and refactor server setup

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+// Path: server\config\db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit the process if MongoDB connection fails
+  }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 // Path: server\server.js
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
 require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.use(cors());
 app.use(express.json());
