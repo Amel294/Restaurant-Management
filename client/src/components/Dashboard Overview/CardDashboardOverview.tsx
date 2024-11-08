5b8ac5401b1217485c36fff484184cdf83366ab1
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-08 23:14:55 +0530

    style(client): enhance responsive layout for DashboardOverview and CardDashboardOverview

--- client/src/components/Dashboard Overview/CardDashboardOverview.tsx
+++ client/src/components/Dashboard Overview/CardDashboardOverview.tsx
@@ -20,17 +20,23 @@ function CardDashboardOverview( { icon, title, value, change, backgroundColor }:
     const changeColor = change.isIncrease ? 'bg-green-300' : 'bg-red-300';
 
     return (
-        <div className={`bg-white p-3 w-full flex items-start rounded-lg  ${ backgroundColor }`}>
-            <div className='flex items-center p-5 bg-green-200 rounded-full m-6 '>{icon}</div>
-            <div className='py-4'>
+        <div
+            className={`bg-white p-5 w-full rounded-lg ${backgroundColor} grid gap-4 items-center sm:grid-cols-[auto,1fr]`}
+        >
+            <div className="flex items-center justify-center p-5 bg-green-200 rounded-full">
+                {icon}
+            </div>
+
+            <div className="flex flex-col items-center sm:items-start text-center sm:text-left">
                 <div className="text-4xl font-extrabold">{value}</div>
-                <div className='text-sm py-1'>{title}</div>
-                <div className="flex flex-row items-center gap-2">
-                    <div className={`${ changeColor } flex items-center rounded-full p-1`}>
-                        {changeIcon }
+                <div className="text-sm py-2">{title}</div>
+                <div className="flex items-center gap-2 mt-3">
+                    <div className={`${changeColor} flex items-center justify-center rounded-full p-2`}>
+                        {changeIcon}
                     </div>
-                    <div className='text-xxs'>
-                        <span>{change.value}</span><span>{change.percent}%</span> <span>({change.period})</span>
+                    <div className="text-xs">
+                        <span>{change.percent}%</span> 
+                        <span> (<span>{change.value}</span> {change.period})</span>
                     </div>
                 </div>
             </div>
--- client/src/components/Dashboard Overview/DashboardOverview.tsx
+++ client/src/components/Dashboard Overview/DashboardOverview.tsx
@@ -5,9 +5,9 @@ import CardDashboardOverview from './CardDashboardOverview';
 
 function DashboardOverview() {
   return (
-    <div className="grid grid-cols-4 gap-4 pt-10 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
       {CardDashboardOverviewData.map((data, index) => (
-        <CardDashboardOverview key={index} {...data} /> 
+        <CardDashboardOverview key={index} {...data} />
       ))}
     </div>
   );
