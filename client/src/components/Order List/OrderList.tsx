103d52261d06d4b714a2f7343c46bd6fb58a60db
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-09 03:42:16 +0530

    style(client):Update OrderList component: changed 'mt-10' to 'my-10'

--- client/src/components/Order List/OrderList.tsx
+++ client/src/components/Order List/OrderList.tsx
@@ -15,7 +15,7 @@ function OrderList() {
       // Handle adding order to the backend or state here
     };
     return (
-        <div className="bg-white py-5 mt-10  rounded-xl">
+        <div className="bg-white py-5 my-10  rounded-xl">
             <div className="flex justify-between pe-10 items-center pt-3">
                 <div className="text-md font-bold ps-10">Order List</div>
                 <button className="p-2 bg-green-100 rounded-lg text-green-700 flex gap-2" onClick={showModal}><ListPlus /> Add Orders</button>
