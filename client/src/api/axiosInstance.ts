abf0478b6bba0619470f1e41d21a682b5e91ddc6
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-08 11:07:00 +0530

    feat(client): set up Axios instance , install Axios and Ant Design

---
+++ client/src/api/axiosInstance.ts
@@ -0,0 +1,11 @@
+// Path: client\src\api\axiosInstance.ts
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
